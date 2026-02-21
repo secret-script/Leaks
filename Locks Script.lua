f59ea3a19601d5849b4e48b0f0f2d03bdd97ace1
Ice <priyakorn.pj@gmail.com>
2026-02-21 20:46:53 +0700

    Create Locks Script.lua

---
+++ Locks Script.lua
@@ -0,0 +1,77 @@
+-- JUJU HUB - SILENT AIM HOOK (DECRYPTED)
+
+local MainEventName = "MainEvent"
+local UpdateMousePos = "UpdateMousePos"
+local MousePos = "MousePos"
+local Prediction = 0.165
+
+local mt = getrawmetatable(game)
+local oldNamecall = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local args = {...}
+
+    if _G.SilentEnabled
+    and not checkcaller()
+    and method == "FireServer"
+    and self.Name == MainEventName then
+        
+        if args[1] == UpdateMousePos or args[1] == MousePos then
+            
+            local target = game.Players:FindFirstChild(_G.TargetName)
+            if target and target.Character then
+                local hrp = target.Character:FindFirstChild("HumanoidRootPart")
+                
+                if hrp then
+                    -- Prediction aim
+                    args[2] = hrp.Position + (hrp.Velocity * Prediction)
+                    return oldNamecall(self, unpack(args))
+                end
+            end
+        end
+    end
+
+    return oldNamecall(self, ...)
+end)
+
+setreadonly(mt, true)
+
+
+
+
+
+
+--[[
+-- [[ JUJU_HUB_HEX_INTERFACE ]]
+local _0x4D61696E = "\77\97\105\110\69\118\101\110\116"
+local _0x55706461 = "\85\112\100\97\116\101\77\111\117\115\101\80\111\115"
+local _0x4D6F7573 = "\77\111\117\115\101\80\111\115"
+local _0x30313635 = (0.33/2) -- 0.165
+
+local _0x4D54 = getrawmetatable(game)
+local _0x4E43 = _0x4D54.__namecall
+setreadonly(_0x4D54, false)
+
+_0x4D54.__namecall = newcclosure(function(self, ...)
+    local _0x4D = getnamecallmethod()
+    local _0x41 = {...}
+    
+    if _G.SilentEnabled and not checkcaller() and _0x4D == "\70\105\114\101\83\101\114\118\101\114" and self.Name == _0x4D61696E then
+        if _0x41[1] == _0x55706461 or _0x41[1] == _0x4D6F7573 then
+            local _0x50 = game.Players:FindFirstChild(_G.TargetName)
+            if _0x50 and _0x50.Character then
+                local _0x48 = _0x50.Character:FindFirstChild("\72\117\109\97\110\111\105\100\82\111\111\116\80\97\114\116")
+                if _0x48 then
+                    _0x41[2] = _0x48.Position + (_0x48.Velocity * _0x30313635)
+                    return _0x4E43(self, unpack(_0x41))
+                end
+            end
+        end
+    end
+    return _0x4E43(self, ...)
+end)
+
+setreadonly(_0x4D54, true)
+]]
